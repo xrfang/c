3bdf6b39202295f8570f090614d10c04be6372e6
xrfang <xrfang@gmail.com>
2020-04-19 11:48:28 +0800

    map: added document in map.h

--- map.h
+++ map.h
@@ -1,37 +1,78 @@
+/*
+map -- simple generic map structure
+SPDX-License-Identifier: MIT
+Copyright (C) 2020, Xiangrong Fang
+Homepage: https://github.com/xrfang/c
+*/
 #ifndef MAP_H
 #define MAP_H
 
 #include <stddef.h>
 
+//iterator, used by map_walk()
+//return non-zero will cause map_walk() to abort iteration
 typedef int (*map_iter)(const void *, size_t);
+
+//comparator, used by map_find() and map_find_addr()
+//signature same as memcmp()
 typedef int (*map_cmp)(const void *, const void *, size_t);
+
+//the Map structure is defined in map.c
 typedef struct Map Map;
 
+//initialize a map structure with item_len.  Internally,
+//Map is implemented as a dynamic array, with elements of
+//same length.
 Map *map_init(size_t item_len);
+
+//clear map buffer, reseet count and capacity to 0
+//map is still usable after clear.
+void map_clear(Map *m);
+//clear map buffer and release the map itself
 void map_free(Map *m);
 
+//return capacity of map buffer (in term of item count)
+//for memory occupation, time the value by item_length
 size_t map_capacity(Map *m);
+//return number of items in the map
 size_t map_count(Map *m);
-void map_clear(Map *m);
 
-//0=inserted; 1=updated; -1=out-of-memory
+//add item into the map. return 0 if the item was not
+//in the map; 1 if the item was there and updated; -1
+//for out-of-memory.
 int map_add(Map *m, void *item);
+//add address of item into the map.
 int map_add_addr(Map *m, void *item);
 
-//0=not found; 1=deleted
+//delete item from map. return 0 if item was not found;
+//1 if item was found and deleted.
 int map_del(Map *m, void *item);
+//delete address of item from map.
 int map_del_addr(Map *m, void *item);
 
-//NULL=not found; otherwise pointer to item
-//idx: if not NULL, store position of item found/to insert
+//search for item in the map. if found, return pointer
+//to the item, otherwise NULL. the second argument idx
+//is optional. if not NULL, returns the position of 
+//item found or to be inserted.  position is 0-based.
 void *map_find(const Map *m, void *key, size_t *idx);
+//search for address of item in the map.
 void *map_find_addr(const Map *m, void *key, size_t *idx);
 
-//0=success; 1=cap too small; -1=out-of-memory
+//set map capacity. return 0 if successful; 1 means
+//new capacity is too small for the items currently
+//in map; -1 means out of memory.
 int map_setcap(Map *m, size_t cap);
-void map_setcmp(Map *m, map_cmp cmp);
 
+//set comparator for the map. by default, map uses
+//memcmp as comparator.
+void map_setcmp(Map *m, map_cmp cmp);
+//string comparator provided for convenience.
 int map_cmpstr(const void *haystack, const void *needle, size_t len);
+
+//emit each item in the map to the iterator. return 0
+//if all items are traversed.  if the iterator returns
+//non-zero, the process is aborted, and that value is
+//returned.
 int map_walk(Map *m, map_iter iter);
 
 #endif
